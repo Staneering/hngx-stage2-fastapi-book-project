5d709427a03c9251d44225909640f8205cedb309
unknown <stacymacbrains@gmail.com>
2025-02-12 00:38:46 +0100

    build:build automation test

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -63,3 +63,9 @@ async def delete_book(book_id: int) -> None:
 
 
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.books.get(book_id)
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Book {book_id} not found")
+    return book
